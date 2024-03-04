90066a0c476c1cef66c446b4ab2db44f700379f0
PlotMasterKoushik <156508822+PlotMasterKoushik@users.noreply.github.com>
2024-03-04 15:08:56 +0000

    done

---
+++ script.js
@@ -0,0 +1,30 @@
+async function fetchData() {
+    try {
+      const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+      const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`);
+  
+      if (!response.ok) {
+        throw new Error("Could not fetch resource");
+      }
+  
+      const data = await response.json();
+      console.log(data);
+  
+      const pokemonSprite = data.sprites.front_default;
+      const imgElement = document.getElementById("PokemonImage");
+
+      const abilitiesList = document.getElementById("Abilities");
+      abilitiesList.innerHTML = "";
+      data.abilities.forEach((ability) => {
+        const listItem = document.createElement("li");
+        listItem.textContent = ability.ability.name;
+        abilitiesList.appendChild(listItem);
+      });
+  
+      imgElement.src = pokemonSprite;
+      imgElement.style.display = "block";
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
